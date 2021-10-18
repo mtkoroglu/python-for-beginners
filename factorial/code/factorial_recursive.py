3303f408f7316c499c5ff5eba075cda67ac499a0
mtkoroglu <m.tahakoroglu@gmail.com>
2021-10-18 23:58:01 +0300

    Add files via upload

--- factorial/code/factorial_recursive.py
+++ factorial/code/factorial_recursive.py
@@ -1,12 +1,8 @@
 def fac(n):
-    if n == 0:
+    if n == 0 or n == 1:
         return 1
     else:
-        product = 1
-        while n > 0:
-            product = n*product
-            n = n-1
-        return product
+        return n*fac(n-1)
 x = int(input('Enter a number: '))
 while x < 0:
     x = int(input('You cannot enter a negative number. Enter a number again: '))
