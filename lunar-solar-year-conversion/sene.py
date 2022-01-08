e5920caa29a68a8bd0dda8838a894a546aa0844a
mtkoroglu <m.tahakoroglu@gmail.com>
2022-01-08 18:52:49 +0300

    Add files via upload

--- lunar-solar-year-conversion/sene.py
+++ lunar-solar-year-conversion/sene.py
@@ -1,1 +1,12 @@
-
+def lunar_to_solar(x):
+    return round((x*354)/365)
+def solar_to_lunar(x):
+    return round((x*365)/354)
+print('Ay takviminden güneş takvimine sene dönüşümü için 1')
+print('Güneş takviminden ay takvimine sene dönüşümü için 2')
+secim = int(input('Seçiminizi giriniz: '))
+sene = int(input('Kaç sene? '))
+if (secim == 1):
+    print('%i ay senesi = %i güneş senesi' %(sene, lunar_to_solar(sene)))
+elif (secim == 2):
+    print('%i güneş senesi = %i ay senesi' %(sene, solar_to_lunar(sene)))
